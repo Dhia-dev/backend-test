0dea87a76bbbb54ba4e5212bd845087527be8aa2
Dhia-dev <dhia.mejri@esprit.tn>
2024-12-02 00:13:54 +0100

    fix

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    cb(null, `uploads/${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
   }
 });
 
